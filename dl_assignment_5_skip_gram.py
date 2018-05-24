216eb4e9642c267ac9ff1d3bc7847aa95fb0ddf9
mr-cloud <zhangxin1992pm@gmail.com>
2018-05-24 08:41:26 +0800

    Comment skip-gram

--- dl_assignment_5_skip_gram.py
+++ dl_assignment_5_skip_gram.py
@@ -1,4 +1,4 @@
-#%matplotlib inline
+# %matplotlib inline
 from __future__ import print_function
 import collections
 import math
@@ -12,24 +12,26 @@ from six.moves import range
 from six.moves.urllib.request import urlretrieve
 from sklearn.manifold import TSNE
 
-
 url = 'http://mattmahoney.net/dc/'
 
+
 def maybe_download(filename, expected_bytes):
-  """Download a file if not present, and make sure it's the right size."""
-  if not os.path.exists(filename):
-    filename, _ = urlretrieve(url + filename, filename)
-  statinfo = os.stat(filename)
-  if statinfo.st_size == expected_bytes:
-    print('Found and verified %s' % filename)
-  else:
-    print(statinfo.st_size)
-    raise Exception(
-      'Failed to verify ' + filename + '. Can you get to it with a browser?')
-  return filename
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified %s' % filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception(
+            'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
+
 
 # filename = maybe_download('text8.zip', 31344016)
 filename = 'text8.zip'
+
 
 def read_data(filename):
     """Extract the first file enclosed in a zip file as a list of words"""
@@ -41,61 +43,70 @@ def read_data(filename):
 words = read_data(filename)
 print('Data size %d' % len(words))
 
-
 vocabulary_size = 50000
 
 
 def build_dataset(words):
-  count = [['UNK', -1]]
-  count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
-  dictionary = dict()
-  for word, _ in count:
-    dictionary[word] = len(dictionary)  # element is labeled with (word, index).
-  data = list()  # code for words.
-  unk_count = 0
-  for word in words:
-    if word in dictionary:
-      index = dictionary[word]
-    else:
-      index = 0  # dictionary['UNK']
-      unk_count = unk_count + 1
-    data.append(index)
-  count[0][1] = unk_count
-  reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))  # element is labeled with (index, word)
-  return data, count, dictionary, reverse_dictionary
+    """
+
+    :param words:
+    :return:
+    data -- list of indices for words
+    count -- list of tuples contains <word, count>
+    dictionary -- {word, index}
+    reverse_dictionary -- {index, word}
+    """
+    count = [['UNK', -1]]  # unknown, uncommon
+    count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)  # element is labeled with (word, index).
+    data = list()  # code for words.
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0  # dictionary['UNK']
+            unk_count = unk_count + 1
+        data.append(index)
+    count[0][1] = unk_count
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))  # element is labeled with (index, word)
+    return data, count, dictionary, reverse_dictionary
+
 
 data, count, dictionary, reverse_dictionary = build_dataset(words)
 print('Most common words (+UNK)', count[:5])
 print('Sample data', data[:10])
 del words  # Hint to reduce memory.
 
-
 data_index = 0
 
+
 def generate_batch(batch_size, num_skips, skip_window):
-  global data_index
-  assert batch_size % num_skips == 0  # trigger a warning if false.
-  assert num_skips <= 2 * skip_window # make sure the samples are within the span.
-  #  The bigger num_skips, the less stronger context.
-  batch = np.ndarray(shape=(batch_size), dtype=np.int32)
-  labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
-  span = 2 * skip_window + 1 # [ skip_window target skip_window ]
-  buffer = collections.deque(maxlen=span)
-  for _ in range(span):
-    buffer.append(data[data_index])
-    data_index = (data_index + 1) % len(data)  # learn repeatedly.
-  for i in range(batch_size // num_skips):
-    target = skip_window  # target label at the center of the buffer
-    targets_to_avoid = [ skip_window ]
-    for j in range(num_skips):
-      while target in targets_to_avoid:
-        target = random.randint(0, span - 1)
-      targets_to_avoid.append(target) # avoid to reuse the input as the label.
-      batch[i * num_skips + j] = buffer[skip_window]
-      labels[i * num_skips + j, 0] = buffer[target]
-    buffer.append(data[data_index])  # update the center word and make a continuous context training.
-    data_index = (data_index + 1) % len(data)
-  return batch, labels
+    global data_index
+    assert batch_size % num_skips == 0  # trigger a warning if false.
+    assert num_skips <= 2 * skip_window  # make sure the samples are within the span.
+    #  The bigger num_skips, the less stronger context.
+    batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+    span = 2 * skip_window + 1  # [ skip_window target skip_window ]
+    buffer = collections.deque(maxlen=span)
+    for _ in range(span):
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)  # learn repeatedly.
+    for i in range(batch_size // num_skips):
+        target = skip_window  # target label at the center of the buffer
+        targets_to_avoid = [skip_window]
+        for j in range(num_skips):
+            while target in targets_to_avoid:
+                target = random.randint(0, span - 1)
+            targets_to_avoid.append(target)  # avoid to reuse the input as the label.
+            batch[i * num_skips + j] = buffer[skip_window]
+            labels[i * num_skips + j, 0] = buffer[target]
+        buffer.append(data[data_index])  # update the center word and make a continuous context training.
+        data_index = (data_index + 1) % len(data)
+    return batch, labels
 
 
 # demo for batch data generation.
@@ -107,7 +118,6 @@ for num_skips, skip_window in [(2, 1), (4, 2)]:
     print('\nwith num_skips = %d and skip_window = %d:' % (num_skips, skip_window))
     print('    batch:', [reverse_dictionary[bi] for bi in batch])
     print('    labels:', [reverse_dictionary[li] for li in labels.reshape(8)])
-
 
 batch_size = 128
 embedding_size = 128  # Dimension of the embedding vector.
@@ -161,58 +171,60 @@ with graph.as_default(), tf.device('/cpu:0'):
     valid_embeddings = tf.nn.embedding_lookup(
         normalized_embeddings, valid_dataset)
     #
-    similarity = tf.matmul(valid_embeddings, tf.transpose(normalized_embeddings))  # cosine similarity between the two vectors.
-
+    similarity = tf.matmul(valid_embeddings,
+                           tf.transpose(normalized_embeddings))  # cosine similarity between the two vectors.
 
     num_steps = 100001
 
     with tf.Session(graph=graph) as session:
-      tf.global_variables_initializer().run()
-      print('Initialized')
-      average_loss = 0
-      for step in range(num_steps):
-        batch_data, batch_labels = generate_batch(
-          batch_size, num_skips, skip_window)
-        feed_dict = {train_dataset : batch_data, train_labels : batch_labels}
-        _, l = session.run([optimizer, loss], feed_dict=feed_dict)
-        average_loss += l
-        if step % 2000 == 0:
-          if step > 0:
-            average_loss = average_loss / 2000
-          # The average loss is an estimate of the loss over the last 2000 batches.
-          print('Average loss at step %d: %f' % (step, average_loss))
-          average_loss = 0
-        # note that this is expensive (~20% slowdown if computed every 500 steps)
-        if step % 10000 == 0:
-          sim = similarity.eval()
-          for i in range(valid_size):
-            valid_word = reverse_dictionary[valid_examples[i]]
-            top_k = 8 # number of nearest neighbors
-            # the first element in the ordered sim mst be the example itselt cause the angle is 0 degree.
-            nearest = (-sim[i, :]).argsort()[1:top_k+1]  # the larger, the closer which means the angle is smaller.
-            log = 'Nearest to %s:' % valid_word
-            for k in range(top_k):
-              close_word = reverse_dictionary[nearest[k]]  # link all the nearest neighbors.
-              log = '%s %s,' % (log, close_word)
-            print(log)
-      final_embeddings = normalized_embeddings.eval()
+        tf.global_variables_initializer().run()
+        print('Initialized')
+        average_loss = 0
+        for step in range(num_steps):
+            batch_data, batch_labels = generate_batch(
+                batch_size, num_skips, skip_window)
+            feed_dict = {train_dataset: batch_data, train_labels: batch_labels}
+            _, l = session.run([optimizer, loss], feed_dict=feed_dict)
+            average_loss += l
+            if step % 2000 == 0:
+                if step > 0:
+                    average_loss = average_loss / 2000
+                # The average loss is an estimate of the loss over the last 2000 batches.
+                print('Average loss at step %d: %f' % (step, average_loss))
+                average_loss = 0
+            # note that this is expensive (~20% slowdown if computed every 500 steps)
+            if step % 10000 == 0:
+                sim = similarity.eval()
+                for i in range(valid_size):
+                    valid_word = reverse_dictionary[valid_examples[i]]
+                    top_k = 8  # number of nearest neighbors
+                    # the first element in the ordered sim mst be the example itselt cause the angle is 0 degree.
+                    nearest = (-sim[i, :]).argsort()[
+                              1:top_k + 1]  # the larger, the closer which means the angle is smaller.
+                    log = 'Nearest to %s:' % valid_word
+                    for k in range(top_k):
+                        close_word = reverse_dictionary[nearest[k]]  # link all the nearest neighbors.
+                        log = '%s %s,' % (log, close_word)
+                    print(log)
+        final_embeddings = normalized_embeddings.eval()
 
-      num_points = 400
+        num_points = 400
 
-      tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)  # less stable than PCA etc. But good distance virtualization.
-      two_d_embeddings = tsne.fit_transform(final_embeddings[1:num_points + 1, :])
-
-
-      def plot(embeddings, labels):
-          assert embeddings.shape[0] >= len(labels), 'More labels than embeddings'
-          pylab.figure(figsize=(15, 15))  # in inches
-          for i, label in enumerate(labels):
-              x, y = embeddings[i, :]
-              pylab.scatter(x, y)
-              pylab.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points',
-                             ha='right', va='bottom')
-          pylab.show()
+        tsne = TSNE(perplexity=30, n_components=2, init='pca',
+                    n_iter=5000)  # less stable than PCA etc. But good distance virtualization.
+        two_d_embeddings = tsne.fit_transform(final_embeddings[1:num_points + 1, :])
 
 
-      words = [reverse_dictionary[i] for i in range(1, num_points + 1)]
-      plot(two_d_embeddings, words)
+        def plot(embeddings, labels):
+            assert embeddings.shape[0] >= len(labels), 'More labels than embeddings'
+            pylab.figure(figsize=(15, 15))  # in inches
+            for i, label in enumerate(labels):
+                x, y = embeddings[i, :]
+                pylab.scatter(x, y)
+                pylab.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points',
+                               ha='right', va='bottom')
+            pylab.show()
+
+
+        words = [reverse_dictionary[i] for i in range(1, num_points + 1)]
+        plot(two_d_embeddings, words)
--- softmax.py
+++ softmax.py
@@ -19,6 +19,7 @@ import matplotlib.pyplot as plt
 x = np.arange(-2.0, 6.0, 0.1)
 scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
 
-plt.plot(x, softmax(scores).T, linewidth=2)
+y = softmax(scores).T
+plt.plot(x, y, linewidth=2)
 plt.show()
 
